be905fe26c6c7ef23717514f2510ac4db750b12e
1393429978@qq.com <1393429978@qq.com>
2023-12-20 23:32:59 +0800

    first commit

---
+++ lab3_data_scratch.py
@@ -0,0 +1,125 @@
+from sklearn.model_selection import train_test_split
+import os
+import cv2 as cv
+import torch
+from loguru import logger
+from torch.utils.data import DataLoader as dtl
+
+
+class VideoDataset(torch.utils.data.Dataset):
+    '''
+    Custom Dataset for loading videos and their class labels
+    '''
+
+    def __init__(self, data_dir, num_classes=10, num_frames=20, transform=None, target_transform=None):
+        super().__init__()
+
+        self.data_dir = data_dir
+
+        self.transform = transform
+        self.target_transform = target_transform
+        self.num_classes = num_classes
+        self.num_frames = num_frames
+
+        self.video_filename_list = []
+        self.classesIdx_list = []
+
+        self.class_dict = {class_label: idx for idx, class_label in enumerate(
+            sorted(os.listdir(self.data_dir)))}
+
+        for class_label, class_idx in self.class_dict.items():
+            class_dir = os.path.join(self.data_dir, class_label)
+            for video_filename in sorted(os.listdir(class_dir)):
+                self.video_filename_list.append(
+                    os.path.join(class_label, video_filename))
+                self.classesIdx_list.append(class_idx)
+
+    def __len__(self):
+        return len(self.video_filename_list)
+
+    def read_video(self, video_path):
+        frames = []
+        cap = cv.VideoCapture(video_path)
+        count_frames = 0
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                if self.transform:
+                    transformed = self.transform(image=frame)
+                    frame = transformed['image']
+
+                frames.append(frame)
+                count_frames += 1
+            else:
+                break
+
+        stride = count_frames // self.num_frames
+        new_frames = []
+        count = 0
+        for i in range(0, count_frames, stride):
+            if count >= self.num_frames:
+                break
+            new_frames.append(frames[i])
+            count += 1
+
+        cap.release()
+
+        return torch.stack(new_frames, dim=0)
+
+    def __getitem__(self, idx):
+        classIdx = self.classesIdx_list[idx]
+        video_filename = self.video_filename_list[idx]
+        video_path = os.path.join(self.data_dir, video_filename)
+        frames = self.read_video(video_path)
+        return frames, classIdx
+
+
+def transform_data(num_class, batch_sz, num_frame, num_workers, data_dir, transform_fun):
+    full_dataset = VideoDataset(data_dir=data_dir, num_frames=num_frame, num_classes=num_class, transform=transform_fun)
+    train, test = train_test_split(full_dataset, test_size=0.2, random_state=42)
+    train_loader = dtl(train, batch_size=batch_sz, shuffle=True, num_workers=num_workers)
+    test_loader = dtl(test, batch_size=batch_sz, shuffle=False, num_workers=num_workers)
+    return train_loader, test_loader
+
+
+def split_dataloader(train_data, validation_split=0.2):
+
+    train_ratio = 1 - validation_split
+    train_size = int(train_ratio * len(train_data.dataset))
+
+    indices = list(range(len(train_data.dataset)))
+    train_indices = indices[:train_size]
+    val_indices = indices[train_size:]
+
+    dataset = train_data.dataset
+    batch_size = train_data.batch_size
+    num_workers = train_data.num_workers
+
+    train_sampler = torch.utils.data.SubsetRandomSampler(train_indices)
+    val_sampler = torch.utils.data.SubsetRandomSampler(val_indices)
+
+    train_data = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler,
+                                             num_workers=num_workers, drop_last=True)
+    val_data = torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=val_sampler, num_workers=num_workers,
+                                           drop_last=True)
+
+    return train_data, val_data
+
+# if __name__ == '__main__':
+#
+#     logger.info('Loading dataset')
+#     full_dataset = VideoDataset(data_dir="data", num_frames=num_frames, num_classes=num_classes, transform=transform)
+#
+#     train_dataset, test_dataset = train_test_split(full_dataset, test_size=0.2, random_state=42)
+#     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+#                                                num_workers=num_workers)
+#     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
+#                                               num_workers=num_workers)
+#     logger.info('Dataset loaded')
+#
+#     for batch_idx, (data, target) in enumerate(train_loader):
+#         print(data.shape)
+#         print(target.shape)
+#         break
+
+# Hint: refer to https://www.kaggle.com/code/nguyenmanhcuongg/pytorch-video-classification-with-conv2d-lstm to implement your model and other functions
---
+++ main.py
@@ -0,0 +1,42 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import lab3_data_scratch
+import my_model, pretrained_model
+import torch.nn as nn
+import train_eva
+import visual_history
+
+num_classes = 10
+batch_size = 4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+num_frames = 20  # You can adjust this to balance speed and accuracy
+img_size = (128, 128)  # You can adjust this to balance speed and accuracy
+num_workers = 4
+last_weights = 'last_weights.pt'
+best_weights = 'best_weights.pt'
+transform = A.Compose(
+    [
+        A.Resize(height=img_size[0], width=img_size[1]),
+        A.Normalize(),
+        ToTensorV2()
+    ]
+)
+
+if __name__ == '__main__':
+    train_, test_ = lab3_data_scratch.transform_data(num_classes, batch_size, num_frames, num_workers, './data',
+                                                   transform)
+    for batch_idx, (data, target) in enumerate(train_):
+        print(data.shape)
+        print(target.shape)
+        break
+    model = my_model.MyModel(num_classes=num_classes, hidden_size=128, num_lstm=2)
+    loss_fn = nn.CrossEntropyLoss()
+    opt = torch.optim.Adam(model.parameters(), lr=1e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, factor=0.1, mode='min', patience=3, verbose=True)
+    model, his = train_eva.train(model, train_, loss_fn, opt, weights=None, epochs=50,
+                                 save_best_weights_path=last_weights, save_last_weights_path=best_weights, device=device, validation_split=0.2, steps_per_epoch=100, scheduler=scheduler)
+    visual_history.visualize_history(his)
+    test_loss, test_acc = train_eva.evaluate(model, weights=last_weights, val_data=test_, loss_fn=loss_fn, device='cuda', verbose=True)
+    print(f'Loss: {test_loss : .3f}, Acc: {test_acc: .3f}')
+
