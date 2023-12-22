bd3046d0ec7a67585d70bcb053c8a67aa006b62e
1393429978@qq.com <1393429978@qq.com>
2023-12-22 23:51:15 +0800

    fix 1

--- my_model.py
+++ my_model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class MyModel(torch.nn.Module):
     def __init__(self, num_classes, hidden_size, num_lstm=2):
         super().__init__()
-        self.Conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.Conv1 = nn.Conv2d(in_channels=3, out_channels=128, kernel_size=3, padding=1)
         self.Relu1 = nn.ReLU(inplace=True)
         self.Convadd = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.MaxPooling1 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -15,7 +15,7 @@ class MyModel(torch.nn.Module):
         self.Pooling3 = nn.AdaptiveMaxPool2d((2, 2))
         self.LSTM = nn.LSTM(input_size=2048, hidden_size=hidden_size, num_layers=num_lstm, batch_first=True)
         self.FC = nn.Linear(hidden_size, num_classes)
-        self.SoftMax = nn.Softmax(dim=1)
+        # self.SoftMax = nn.Softmax(dim=1)
         self.Conv = nn.Sequential(
             self.Conv1, self.Relu1, self.MaxPooling1, self.Conv2,
             self.Relu1, self.MaxPooling2, self.Conv3, self.Relu1, self.Pooling3
@@ -24,12 +24,17 @@ class MyModel(torch.nn.Module):
             self.Conv1, self.Relu1, self.Convadd, self.Relu1, self.MaxPooling1, self.Conv2,
             self.Relu1, self.MaxPooling2, self.Conv3, self.Relu1, self.Pooling3
         )
+        self.Conv_2 = nn.Sequential(
+            self.Conv1, self.Relu1, self.MaxPooling1, self.Conv3,
+            self.Relu1, self.Pooling3
+        )
 
     def forward(self, x):
         x1, x2, x3, x4, x5 = x.shape
         x = torch.reshape(x, (-1, *x.shape[2:]))
         # x = self.Conv(x)
-        x = self.Conv_1(x)
+        # x = self.Conv_1(x)
+        x = self.Conv_2(x)
         x = nn.Flatten()(x)
         x = torch.reshape(x, (x1, x2, -1))
         x, (h_n, c_n) = self.LSTM(x)
