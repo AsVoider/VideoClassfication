b00a750340853d158679bc63672d4e6096a7a3f5
1393429978@qq.com <1393429978@qq.com>
2023-12-23 14:52:37 +0800

    fix 1

--- main.py
+++ main.py
@@ -29,13 +29,13 @@ if __name__ == '__main__':
     train_, val_, test_ = lab3_data_scratch.transform_data(num_classes, batch_size, num_frames, num_workers,
                                                            './data', transform)
     model = my_model.MyModel(num_classes=num_classes, hidden_size=128, num_lstm=2) if args.model_select == "my_model" \
-        else pretrained_model.ClassificationModel(num_classes=num_classes, hidden_size=128, num_lstm_layers=3)
+        else pretrained_model.ClassificationModel(num_classes=num_classes, hidden_size=128, num_lstm_layers=2)
     loss_fn = nn.CrossEntropyLoss()
     opt = torch.optim.Adam(model.parameters(), lr=1e-4)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(opt, factor=0.1, mode='min', patience=3,
                                                            verbose=True) if args.lr_select == 're' \
         else torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(opt, 20, 1, 1e-6, verbose=True)
-    model, his = train_eva.train(model, train_, loss_fn, opt, weights=None, epochs=50, validation_data=val_,
+    model, his = train_eva.train(model, train_, loss_fn, opt, weights=None, epochs=100, validation_data=val_,
                                  save_best_weights_path=best_weights, save_last_weights_path=last_weights,
                                  device=device, validation_split=None, steps_per_epoch=100, scheduler=scheduler)
     visual_history.visualize_history(his)
